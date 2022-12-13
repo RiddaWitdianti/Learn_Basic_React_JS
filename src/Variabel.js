e8728ebe0b92049e9b3461ffbd691a3b17aed028
Ridda Witdianti <witdiantiridda@gmail.com>
2022-12-13 14:26:13 +0700

    first commit

---
+++ src/Variabel.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+//Const
+// const harga = 3000;
+
+//Var
+var harga = 20000;
+if (true) {
+  var harga = 30000;
+}
+
+
+const Variabel = () => {
+  return (
+    <div>
+          <h2>Harga : {harga}</h2>
+    </div>
+  )
+}
+
+export default Variabel
