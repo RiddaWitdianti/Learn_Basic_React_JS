00b139ad58bbc9f15887ca3c5790280896109f4b
Ridda Witdianti <witdiantiridda@gmail.com>
2022-12-23 15:14:11 +0700

    Map, Filter, and Reduce

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
 // import Variabel from './Variabel';
-import StateProps from './StateProps';
+// import StateProps from './StateProps';
+import Map from './Map';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StateProps />
+    <Map />
   </React.StrictMode>
 );
 
