79d549ab39e036adaaa9503d9d5fa20035edbb12
Ridda Witdianti <witdiantiridda@gmail.com>
2023-01-05 18:19:41 +0700

    Lifecycle Component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import App from './App';
 // import Variabel from './Variabel';
 // import StateProps from './StateProps';
-import Map from './Map';
+// import Map from './Map';
+import Lifecycle from "./Lifecycle";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Map />
+    <Lifecycle />
   </React.StrictMode>
 );
-
-
