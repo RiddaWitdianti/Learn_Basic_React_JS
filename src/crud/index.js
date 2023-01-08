f28d835f6f4225891151ae9e2a6698b2688d8634
Ridda Witdianti <witdiantiridda@gmail.com>
2023-01-08 16:19:46 +0700

    CRUD Create dan Read Data

--- src/crud/index.js
+++ src/crud/index.js
@@ -4,13 +4,60 @@ import NavbarComponent from "./NavbarComponent";
 import Tabel from "./Tabel";
 
 export default class Crud extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      makanans: [],
+      nama: "",
+      deskripsi: "",
+      harga: 0,
+      id: "",
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({
+      makanans: [
+        ...this.state.makanans,
+        {
+          id: this.state.makanans.length + 1,
+          nama: this.state.nama,
+          deskripsi: this.state.deskripsi,
+          harga: this.state.harga,
+        },
+      ],
+    });
+
+    this.setState({
+      nama: "",
+      deskripsi: "",
+      harga: 0,
+      id: "",
+    })
+
+  };
+
   render() {
+    // console.log(this.state.makanans);
     return (
       <div>
         <NavbarComponent />
         <div className="container mt-4">
-          <Tabel />
-          <Formulir />
+          <Tabel makanans={this.state.makanans} />
+          <Formulir
+            {...this.state}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+          />
         </div>
       </div>
     );
