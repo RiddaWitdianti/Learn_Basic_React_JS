2add22f46bc49648ce86b7f69025fc2e88e2205e
Ridda Witdianti <witdiantiridda@gmail.com>
2023-01-10 17:19:32 +0700

    CRUD Update Data

--- src/crud/index.js
+++ src/crud/index.js
@@ -25,25 +25,60 @@ export default class Crud extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
 
-    this.setState({
-      makanans: [
-        ...this.state.makanans,
-        {
-          id: this.state.makanans.length + 1,
-          nama: this.state.nama,
-          deskripsi: this.state.deskripsi,
-          harga: this.state.harga,
-        },
-      ],
-    });
+    if (this.state.id === "") {
+      this.setState({
+        makanans: [
+          ...this.state.makanans,
+          {
+            id: this.state.makanans.length + 1,
+            nama: this.state.nama,
+            deskripsi: this.state.deskripsi,
+            harga: this.state.harga,
+          },
+        ],
+      });
+    } else {
+      const makananYangSelainDipilih = this.state.makanans
+        .filter((makanan) => makanan.id !== this.state.id)
+        .map((filterMakanan) => {
+          return filterMakanan;
+        });
+
+      this.setState({
+        makanans: [
+          ...makananYangSelainDipilih,
+          {
+            id: this.state.makanans.length + 1,
+            nama: this.state.nama,
+            deskripsi: this.state.deskripsi,
+            harga: this.state.harga,
+          },
+        ],
+      });
+    }
 
     this.setState({
       nama: "",
       deskripsi: "",
       harga: 0,
       id: "",
-    })
+    });
+  };
 
+  editData = (id) => {
+    // console.log("ID: ", id );
+    const makananYangDipilih = this.state.makanans
+      .filter((makanan) => makanan.id === id)
+      .map((filterMakanan) => {
+        return filterMakanan;
+      });
+
+    this.setState({
+      nama: makananYangDipilih[0].nama,
+      deskripsi: makananYangDipilih[0].deskripsi,
+      harga: makananYangDipilih[0].harga,
+      id: makananYangDipilih[0].id,
+    });
   };
 
   render() {
@@ -52,7 +87,7 @@ export default class Crud extends Component {
       <div>
         <NavbarComponent />
         <div className="container mt-4">
-          <Tabel makanans={this.state.makanans} />
+          <Tabel makanans={this.state.makanans} editData={this.editData} />
           <Formulir
             {...this.state}
             handleChange={this.handleChange}
